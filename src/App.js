72fee0d84e4ee97c2fe32b460fa5c1e48550b1c3
Muhammad Hamza Qureshi <gdphp7@gmail.com>
2024-03-03 14:02:22 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with HamZa
+          Learn React with HamZa 
         </a>
       </header>
     </div>
